402e47cfa4f49ef505c8a2f7a0154d9d3ed380ce
yzde <uwuzide@gmail.com>
2022-12-31 17:42:07 +0100

    uwu

--- main.js
+++ main.js
@@ -1,10 +1,13 @@
 window.addEventListener('load', function() {
     setTimeout(function() {
       document.getElementById('preloader').classList.add('loaded');
+      setTimeout(function() {
+        document.getElementById('subloader').innerHTML = "";
+        document.getElementById('preloader').innerHTML = "";
+        document.getElementById('preloader').remove();  // delete the preloader element
+      }, 500);
     }, 7000);
     setTimeout(function() {
-    document.getElementById('subloader').classList.remove('wait');
+      document.getElementById('subloader').classList.remove('wait');
     }, 2500);
-
-  });
-  
+});
